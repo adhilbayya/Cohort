55290908741c1d6bfd5061c352b49ee9d870a7ee
AdhilBayya <adhilvk445@gmail.com>
2023-11-27 20:12:35 +0530

    Added some rough work

---
+++ JavaScript/rough-work/setTimeOut.js
@@ -0,0 +1,9 @@
+setTimeout(() => {
+  console.log("1st statement");
+}, 5000);
+setTimeout(() => {
+  console.log("2st statement");
+}, 3000);
+setTimeout(() => {
+  console.log("3st statement");
+}, 1000);
