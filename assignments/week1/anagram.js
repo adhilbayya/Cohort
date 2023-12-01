69cd514c02972341e26ade7c1a7cfa354ee7ca26
AdhilBayya <adhilvk445@gmail.com>
2023-12-01 11:36:37 +0530

    Added week 1 assignment easy

---
+++ assignments/week1/anagram.js
@@ -0,0 +1,18 @@
+function isAnagram(str1, str2) {
+  str1 = str1.replace(/\s/g, "").toLowerCase();
+  str2 = str2.replace(/\s/g, "").toLowerCase();
+
+  if (str1.length != str2.length) {
+    return false;
+  }
+
+  let string1 = str1.split("").sort().join("");
+  let string2 = str2.split("").sort().join("");
+
+  if (string1 == string2) {
+    return true;
+  }
+  return false;
+}
+
+console.log(isAnagram("listen", "siLent"));
