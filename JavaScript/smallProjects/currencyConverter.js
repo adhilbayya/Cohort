80962a0e929839cbd50206597d02b9889d3a162a
adhilbayya <adhilvk445@gmail.com>
2023-12-14 17:20:40 +0530

    Added currency converter

--- JavaScript/smallProjects/currencyConverter.js
+++ JavaScript/smallProjects/currencyConverter.js
@@ -1,17 +1,33 @@
-let axios = require('axios')
+let axios = require("axios");
 
 const exchangeRate = async (fromCurrency, toCurrency) => {
-    const response = await axios.get(`http://data.fixer.io/api/latest?access_key=f68b13604ac8e570a00f7d8fe7f25e1b&format=1`)
-    const rate = response.data.rates;
-    const inr = 1/rate[fromCurrency]
-    const exchangerate = inr * rate[toCurrency]
-    return exchangerate;
-}
+  const response = await axios.get(
+    `http://data.fixer.io/api/latest?access_key=f68b13604ac8e570a00f7d8fe7f25e1b&format=1`
+  );
+  const rate = response.data.rates;
+  const inr = 1 / rate[fromCurrency];
+  const exchangerate = inr * rate[toCurrency];
+  return exchangerate;
+};
 
 const getCountries = async (toCurrency) => {
-    const response = await axios.get(`https://restcountries.com/v3.1/currency/${toCurrency}`)
-    console.log(response.data.name)
-}
+  const response = await axios.get(
+    `https://restcountries.com/v3.1/currency/${toCurrency}`
+  );
+  const countries = response.data.map((resp) => {
+    return resp.name.common;
+  });
+  return countries;
+};
 
-getCountries("india")
+const calculateAmount = async (fromCurrency, toCurrency, amount) => {
+  let exchangeAmount = await exchangeRate(fromCurrency, toCurrency);
+  let countries = await getCountries(fromCurrency);
+  let exchange = (amount * exchangeAmount).toFixed(2);
 
+  return `${amount} ${fromCurrency} is worth ${exchange} ${toCurrency} and can be used in all these contries : ${countries}`;
+};
+
+calculateAmount("USD", "INR", 1).then((message) => {
+  console.log(message);
+});
