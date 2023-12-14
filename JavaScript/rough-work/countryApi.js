0e9b485f80c42105c18ac1c3e904dee92787ae56
adhilbayya <adhilvk445@gmail.com>
2023-12-14 17:21:28 +0530

    Added countries program

--- JavaScript/rough-work/countryApi.js
+++ JavaScript/rough-work/countryApi.js
@@ -1,19 +1,41 @@
-const url = 'https://restcountries.com/v3.1/all';
+// const url = 'https://restcountries.com/v3.1/all';
 
-fetch(url)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+// fetch(url)
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! Status: ${response.status}`);
+//     }
+//     return response.json();
+//   })
+//   .then(data => {
+//     data.forEach(country => {
+//       const name = country.currencies;
+//       console.log('Country Name:', name);
+//       // You can access other country information here as needed
+//     });
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);
+//   });
+fetch("https://restcountries.com/v3.1/all")
+  .then((response) => {
+    // Check if the response status is okay (status code 200)
+    if (response.ok) {
+      // Parse the response JSON data
+      return response.json();
+    } else {
+      throw new Error("Failed to fetch data");
     }
-    return response.json();
   })
-  .then(data => {
-    data.forEach(country => {
-      const name = country.currencies;
-      console.log('Country Name:', name);
-      // You can access other country information here as needed
-    });
+  .then((data) => {
+    // Extract only the country names from the response
+    const countryNames = data.map((country) => country.name.common);
+
+    // Log the array of country names
+    console.log(countryNames);
+
+    // You can further process or use 'countryNames' array here
   })
-  .catch(error => {
-    console.error('Error:', error);
+  .catch((error) => {
+    console.error("Error fetching data:", error);
   });
