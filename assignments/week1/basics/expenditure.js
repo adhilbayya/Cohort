c4db00aa5fc18876320e1a350a37ca276f8e9d20
adhilbayya <adhilvk445@gmail.com>
2023-12-17 00:49:21 +0530

    Added test to the files

--- assignments/week1/basics/expenditure.js
+++ assignments/week1/basics/expenditure.js
@@ -1,4 +1,4 @@
-function expenditureAnalysis(transactions) {
+function calculateTotalSpentByCategory(transactions) {
   let categoryMap = {};
   for (let i = 0; i < transactions.length; i++) {
     let t = transactions[i];
@@ -13,11 +13,12 @@ function expenditureAnalysis(transactions) {
   for (let i = 0; i < keys.length; i++) {
     let category = keys[i];
     let obj = {
-      [category]: categoryMap[category],
+      category: category,
+      totalSpent: categoryMap[category],
     };
     ans.push(obj);
   }
   return ans;
 }
 
-module.exports = expenditureAnalysis;
+module.exports = calculateTotalSpentByCategory;
