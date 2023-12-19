b29504711ac4d48d325857a85e6f79e4a6ceb07c
adhilbayya <adhilvk445@gmail.com>
2023-12-19 20:55:39 +0530

    Added timer with setTimeOut and not setInterval

--- JavaScript/rough-work/timer.js
+++ JavaScript/rough-work/timer.js
@@ -1,8 +1,17 @@
-let counter = 1;
+let counter = 0;
+// function setTimer() {
+//   console.clear();
+//   console.log(counter);
+//   counter += 1;
+// }
+
+// setInterval(setTimer, 1 * 1000);
+
 function setTimer() {
   console.clear();
-  console.log(counter);
   counter += 1;
+  console.log(counter);
+  setTimeout(setTimer, 1000);
 }
 
-setInterval(setTimer, 1 * 1000);
+setTimeout(setTimer, 1000);
