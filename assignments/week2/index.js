40e092f24d92b9c4caf355bc79b2beed2889ecbc
adhilbayya <adhilvk445@gmail.com>
2023-12-26 21:46:30 +0530

    Added some changes

--- assignments/week2/index.js
+++ assignments/week2/index.js
@@ -4,14 +4,15 @@ const port = 3000;
 
 function calcSum(num) {
   let sum = 0;
-  for (let i = 0; i < num; i++) {
+  for (let i = 0; i <= num; i++) {
     sum += i;
   }
   return sum;
 }
 
-app.get("/", (req, res) => {
-  let re = calcSum(100);
+app.get("/calc", (req, res) => {
+  let counter = req.query.calc;
+  let re = calcSum(counter);
   let sum = `The Answer is ${re}`;
   res.send(sum);
 });
